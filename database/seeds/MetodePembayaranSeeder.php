740b828a8bd2285f5b1bb656bc02e62ebbd73c34
aisatriani <aisatriani03@gmail.com>
2016-11-29 22:42:08 +0800

    fix db seed metode pembayaran

--- database/seeds/MetodePembayaranSeeder.php
+++ database/seeds/MetodePembayaranSeeder.php
@@ -14,24 +14,29 @@ class MetodePembayaranSeeder extends Seeder
         \App\MetodePembayaran::create([
             'nama_metode' => 'Loket Pembayaran',
             'desc' => 'Pembayaran hanya dapat dilakukan di loket/kantor atupato',
-            'no_rek' => '-'
+            'no_rek' => '-',
+            'atas_nama' => '-',
         ]);
 
         \App\MetodePembayaran::create([
             'nama_metode' => 'Bayar di tempat',
             'desc' => 'Pembarayan dilakukan di tempat saat pengantaran barang',
-            'no_rek' => '-'
+            'no_rek' => '-',
+            'atasnama' => '-'
+
         ]);
 
         \App\MetodePembayaran::create([
             'nama_metode' => 'Bank Transfer Mandiri',
             'desc' => 'Pembayaran dapat dilakukan melalui ATM/Teller bank BRI',
-            'no_rek' => '1234 234 1342'
+            'no_rek' => '1234 234 1342',
+            'atasnama' => 'Helmiyanto M. Hongi',
         ]);
         \App\MetodePembayaran::create([
             'nama_metode' => 'Bank Transfer BCA',
             'desc' => 'Pembayaran dapat dilakukan melalui ATM/Teller bank BRI',
-            'no_rek' => '234 2435 12312'
+            'no_rek' => '234 2435 12312',
+            'atasnama' => 'Helmiyanto M. Hongi',
         ]);
     }
 }
