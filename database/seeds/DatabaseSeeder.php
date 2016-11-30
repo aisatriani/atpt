3339c3a241384747a42a1b09fdeedd9cbdf67bb8
aisatriani <aisatriani03@gmail.com>
2016-11-30 22:04:17 +0800

    add image_tour table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GallerySeeder::class);
         $this->call(MetodePembayaranSeeder::class);
         $this->call('MediaTableSeeder');
+        $this->call('ImageTourTableSeeder');
     }
 }
